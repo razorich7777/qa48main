26c36adaf8d26fa7fec8e5861df32557f923c46e
sergey <sergiusperlov@gmail.com>
2025-09-13 18:30:10 +0300

    My HW8

--- phonebook/qa48_prhonebook/src/test/java/com/phonebook/AddContactTest.java
+++ phonebook/qa48_prhonebook/src/test/java/com/phonebook/AddContactTest.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
+
 
 public class AddContactTest extends TestBase{
 
